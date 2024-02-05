2bb52785574ac345fee934ad2abe25d53bd6e2d0
Vitor Durante <5401232+vdurante@users.noreply.github.com>
2024-02-05 12:11:08 -0300

    Implementation

--- demos/src/Examples/CollaborativeEditing/React/index.jsx
+++ demos/src/Examples/CollaborativeEditing/React/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import './styles.scss'
 
 import { TiptapCollabProvider } from '@hocuspocus/provider'
@@ -14,37 +15,32 @@ import React, {
   useState,
 } from 'react'
 import * as Y from 'yjs'
+import * as signalR from '@microsoft/signalr'
 
 import { variables } from '../../../variables.js'
 import MenuBar from './MenuBar.jsx'
+import { WebsocketProvider } from 'y-websocket'
+import { WebsocketSignalProvider } from './websocket'
 
 const colors = ['#958DF1', '#F98181', '#FBBC88', '#FAF594', '#70CFF8', '#94FADB', '#B9F18D']
 const names = [
-  'Lea Thompson',
-  'Cyndi Lauper',
-  'Tom Cruise',
-  'Madonna',
-  'Jerry Hall',
-  'Joan Collins',
-  'Winona Ryder',
-  'Christina Applegate',
-  'Alyssa Milano',
-  'Molly Ringwald',
-  'Ally Sheedy',
-  'Debbie Harry',
-  'Olivia Newton-John',
-  'Elton John',
-  'Michael J. Fox',
-  'Axl Rose',
-  'Emilio Estevez',
-  'Ralph Macchio',
-  'Rob Lowe',
-  'Jennifer Grey',
-  'Mickey Rourke',
-  'John Cusack',
-  'Matthew Broderick',
-  'Justine Bateman',
-  'Lisa Bonet',
+  'Vitor',
+  'Nils',
+  'Ian',
+  'Marcello',
+  'Sebastian',
+  'Marci',
+  'Annalisa',
+  'Santi',
+  'Davide',
+  'Andres',
+  'Sam',
+  'Lucas',
+  'Leo',
+  'Lean',
+  'Marek',
+  'Marion',
+  'Mika',
 ]
 
 const getRandomElement = list => list[Math.floor(Math.random() * list.length)]
@@ -57,21 +53,72 @@ const getRandomRoom = () => {
 const getRandomColor = () => getRandomElement(colors)
 const getRandomName = () => getRandomElement(names)
 
-const room = getRandomRoom()
+getRandomRoom()
+// const room = getRandomRoom()
+const room = 'test1234'
 
 const ydoc = new Y.Doc()
-const websocketProvider = new TiptapCollabProvider({
-  appId: '7j9y6m10',
-  name: room,
-  document: ydoc,
-})
+// const websocketProvider = new TiptapCollabProvider({
+//   appId: '7j9y6m10',
+//   name: room,
+//   document: ydoc,
+// })
+// const websocketProvider = new WebsocketProvider('ws://localhost:1234', room, ydoc, {
+//   disableBc: true,
+//   resyncInterval: -1,
+// })
+// const websocketProvider = new WebsocketProvider('ws://localhost:5000/collaboration', room, ydoc, {
+//   disableBc: true,
+//   resyncInterval: 5000,
+// })
 
 const getInitialUser = () => {
-  return JSON.parse(localStorage.getItem('currentUser')) || {
+  // return JSON.parse(localStorage.getItem('currentUser')) || {
+  //   name: getRandomName(),
+  //   color: getRandomColor(),
+  // }
+  return {
     name: getRandomName(),
     color: getRandomColor(),
   }
 }
+
+const docName = 'test999'
+// const signalrUrl = `http://localhost:5000/collab`
+const signalrUrl = `https://api.cwork.io/api/v1/notifications/collab`
+const websocketUrl = `ws://localhost:1234/`
+
+const queryParams = {
+  userId: '710fb749-ddcf-4117-b6c7-daca9356f3c8',
+  workspaceId: '710fb749-ddcf-4117-b6c7-daca9356f3c8',
+  docName: docName,
+  clientId: ydoc.clientID
+}
+
+const queryParamsString = `userid=${queryParams.userId}&workspaceid=${queryParams.workspaceId}&docname=${queryParams.docName}&clientid=${queryParams.clientId}`;
+
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl(`${signalrUrl}?${queryParamsString}`, {
+    // skipNegotiation: true,
+    //transport: signalR.HttpTransportType.WebSockets
+  })
+  .configureLogging(signalR.LogLevel.Debug)
+  .withAutomaticReconnect()
+  .withStatefulReconnect()
+  .build()
+
+const connStart = connection.start()
+
+const signalProvider = new WebsocketSignalProvider(
+  websocketUrl,
+  docName,
+  ydoc,
+  connection,
+  {
+    resyncInterval: 2500,
+    disableBc: true
+  }
+)
 
 export default () => {
   const [status, setStatus] = useState('connecting')
@@ -92,16 +139,28 @@ export default () => {
         document: ydoc,
       }),
       CollaborationCursor.configure({
-        provider: websocketProvider,
+        provider: signalProvider,
       }),
     ],
   })
 
   useEffect(() => {
+    const interval = setInterval(() => {
+      const iframe = document.getElementById('collab-iframe');
+      iframe.src = iframe.src;
+    }, 2500);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
     // Update status changes
-    websocketProvider.on('status', event => {
-      setStatus(event.status)
+    connStart.then(() => {
+      setStatus('connected')
     })
+
+    // websocketProvider.on('status', event => {
+    //   setStatus(event.status)
+    // })
   }, [])
 
   // Save current user to localStorage and emit to editor
@@ -121,19 +180,30 @@ export default () => {
   }, [currentUser])
 
   return (
-    <div className="editor">
-      {editor && <MenuBar editor={editor} />}
-      <EditorContent className="editor__content" editor={editor} />
-      <div className="editor__footer">
-        <div className={`editor__status editor__status--${status}`}>
-          {status === 'connected'
-            ? `${editor.storage.collaborationCursor.users.length} user${editor.storage.collaborationCursor.users.length === 1 ? '' : 's'} online in ${room}`
-            : 'offline'}
-        </div>
-        <div className="editor__name">
-          <button onClick={setName}>{currentUser.name}</button>
+    <div>
+      <div className="editor">
+        {editor && <MenuBar editor={editor}/>}
+        <EditorContent className="editor__content" editor={editor}/>
+        <div className="editor__footer">
+          <div className={`editor__status editor__status--${status}`}>
+            {status === 'connected'
+              ? `${editor.storage.collaborationCursor.users.length} user${editor.storage.collaborationCursor.users.length === 1 ? '' : 's'} online in ${room}`
+              : 'offline'}
+            <br/>
+            client ID {ydoc.clientID}
+          </div>
+          <div className="editor__name">
+            <button onClick={setName}>{currentUser.name}</button>
+          </div>
         </div>
       </div>
+      <iframe src={`https://api.cwork.io/api/v1/notifications/collab`} style={{
+        width: '100%',
+        height: '750px'
+      }}
+              id={'collab-iframe'}
+      >
+      </iframe>
     </div>
-  )
+)
 }
